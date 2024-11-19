6bba3f01261a45b09b31d162c64a448b34e01d96
mmadhura1014 <96589703+mmadhura1014@users.noreply.github.com>
2024-11-19 11:12:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
+  access_key = "AKIA6MOYDAKAH4LHNRET"
+  secret_key = "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"
+}
+
+resource "aws_instance" "m5_instance" {
+  ami           = "ami-012967cc5a8c9f891"
+  instance_type = "c5.metal"
+
+  tags = {
+    Name = "MyM5Instance"
+  }
+}
